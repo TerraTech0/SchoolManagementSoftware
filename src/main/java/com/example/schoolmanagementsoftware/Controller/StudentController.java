509d36694fefdd477c24873ad10b44fb5837afdc
doy7m <d.m1999y@gmail.com>
2024-04-17 05:31:14 +0300

    adding student class and do a relations ManyToMany between student and course classes

--- src/main/java/com/example/schoolmanagementsoftware/Controller/StudentController.java
+++ src/main/java/com/example/schoolmanagementsoftware/Controller/StudentController.java
@@ -8,6 +8,7 @@ import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -27,19 +28,36 @@ public class StudentController {
 
     @PostMapping("/add")
     public ResponseEntity addStudent(@RequestBody @Valid Student student){
+        logger.info("inside add Student");
         studentService.addStudent(student);
         return ResponseEntity.ok().body("student added successfully!");
     }
 
     @PutMapping("/update/{id}")
     public ResponseEntity updateStudent(@PathVariable Integer id, @RequestBody @Valid Student student){
+        logger.info("inside update sutdnet!");
         studentService.updateStudent(id, student);
         return ResponseEntity.ok().body("student updated succesffully!");
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity deleteStudent(@PathVariable Integer id){
+        logger.info("inside delete student!");
         studentService.deleteStudent(id);
         return ResponseEntity.ok().body("student deleted successfully!");
     }
+
+    @PutMapping("/change-student-major/{id}/{newMajor}")
+    public ResponseEntity changeStudentMajor(@PathVariable Integer id, @PathVariable String newMajor){
+        logger.info("inside change student major!");
+        studentService.changeStudentMajor(id, newMajor);
+        return ResponseEntity.ok().body("student's major changed successfully!");
+    }
+
+    @GetMapping("/get/{courseId}")
+    public ResponseEntity getStudentsByCourseId(@PathVariable Integer courseId){
+        logger.info("inside get students by course id!");
+        return ResponseEntity.ok().body(studentService.getStudentsByCourseId(courseId));
+    }
+
 }
